591e5dde4fd4302f252ffb5a5cb16716afafdc7f
CK <cbhimavarapu@datalicious.com>
2017-02-19 18:13:34 +0530

    changing the php path

--- my_testing_script.sh
+++ my_testing_script.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 # php.ini
-echo "memory_limit = 512M" > $PHPENV_ROOT/versions/$(phpenv global)/etc/conf.d/memory.ini
+sudo echo "memory_limit = 512M" > $PHPENV_ROOT/versions/$(phpenv global)/etc/conf.d/memory.ini
 
-echo "always_populate_raw_post_data = -1" > $PHPENV_ROOT/versions/$(phpenv global)/etc/conf.d/deprecated.ini
+sudo echo "always_populate_raw_post_data = -1" > $PHPENV_ROOT/versions/$(phpenv global)/etc/conf.d/deprecated.ini
 
 # echo "xdebug.max_nesting_level=1000" >> /home/rof/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
-rm $(PHPENV_ROOT)/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
+#rm $(PHPENV_ROOT)/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
 
 # uglify
 npm install -g uglify-js
