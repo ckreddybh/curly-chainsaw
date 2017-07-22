219db08ad247ba619e93798de81b8934e12f1891
CK Reddy <chaitu949@gmail.com>
2017-07-22 16:16:37 +0530

    Middleware layer

--- nodejs/webserver/server.js
+++ nodejs/webserver/server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var date = new Date().toString();
+    var log = `${date} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to write to a file');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
