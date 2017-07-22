179b441ced5fead39be24cd9670411ed33c9eca7
CK Reddy <chaitu949@gmail.com>
2017-07-22 13:14:09 +0530

    Adding webserver handelers

---
+++ nodejs/webserver/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (string) => {
+    return string.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to CK mania'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: 'Unable to handle the request'
+    });
+});
+
+app.listen(3003);
